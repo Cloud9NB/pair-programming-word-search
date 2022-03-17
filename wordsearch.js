bb549fa06762df33509d3ef749f330d1f5aef813
Kevin Lee <blackseeds10@hotmail.com>
2022-03-17 04:46:12 +0000

    pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,17 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalArray = letters[0].map((col, index) => letters.map(row => row[index]));
+    const verticalJoin = verticalArray.map(ls => ls.join(''));
 
-module.exports = wordSearch
+    for (const x of horizontalJoin) {
+        if (x.includes(word)) return true;
+        else {
+            for (const y of verticalJoin) {
+                if (y.includes(word)) return true;
+            }
+        }
+    }
+    return false;
+};
+
+module.exports = wordSearch;
